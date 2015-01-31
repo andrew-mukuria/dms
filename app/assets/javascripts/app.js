606f5dbf325d8f360fda004614990c6333a37295
Rufus Mbugua <mbuguarufus@gmail.com>
2015-01-31 15:54:44 +0300

    Removed OLD Bower Components, Added GRUNT and Karma

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,41 @@
+var app = angular.module("hais", ['ui.router','restangular','smart-table','chart.js','textAngular','angularMoment']);
+
+app.controller('benefitsCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+
+  }]);
+
+app.controller('claimsCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+
+  }]);
+
+app.controller('providerCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+
+  }]);
+
+app.controller('servicesCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+
+  }]);
+
+app.config(function(RestangularProvider) {
+  RestangularProvider.setBaseUrl('http://localhost/hmis');
+  RestangularProvider.setRequestSuffix('.json');
+});
+
+app.factory('MembersRestangular', function(Restangular) {
+  return Restangular.withConfig(function(RestangularConfigurer) {
+    RestangularConfigurer.setBaseUrl('http://localhost/hmis/members_api');
+  });
+});
+
+app.run(['$http', '$rootScope', function($http, $rootScope) {
+     $rootScope.date = new Date();
+     $rootScope.title = 'HAIS Web';
+     $rootScope.messages=[];
+     $rootScope.menu=[];
+ }]);
+
+
---
+++ app/assets/javascripts/routes.js
@@ -0,0 +1,134 @@
+app.config(function($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/login");
+    //
+    // Now set up the states
+    $stateProvider
+    .state('login', {
+            url: '/login',
+            templateUrl: 'app/partials/users/login.html',
+            controller: function($rootScope) {
+                $rootScope.date = new Date();
+            }
+        }).
+     state('lock-screen', {
+            url: '/lock-screen',
+            templateUrl: 'app/partials/users/lock-screen.html',
+            controller: function($rootScope) {
+                $rootScope.date = new Date();
+            }
+        }).
+    state('dashboard', {
+        url: '/dashboard',
+        controller: '',
+        templateUrl: 'app/partials/global/dashboard.html'
+    }).
+    state('knowledge-base', {
+        url: '/knowledge-base',
+        controller: function($rootScope) {
+            $rootScope.title = 'Knowledge Base'
+        },
+        templateUrl: 'app/partials/knowledge-base/index.html'
+    }).
+    state('knowledge-base.list', {
+        url: '/list',
+        templateUrl: 'app/partials/knowledge-base/list.html',
+        parent: 'knowledge-base',
+        controller: function($rootScope,$scope) {
+            $rootScope.title = 'Members List',
+            $scope.articles=[];
+        }
+    }).
+    state('knowledge-base.add', {
+        url: '/add',
+        templateUrl: 'app/partials/knowledge-base/form.html',
+        parent: 'knowledge-base',
+        controller: function($rootScope) {
+            $rootScope.title = 'Register Member'
+        }
+    }).
+    state('knowledge-base.view', {
+        url: '/view',
+        templateUrl: 'app/partials/knowledge-base/form.html',
+        parent: 'knowledge-base',
+        controller: function($rootScope) {
+            $rootScope.title = 'View Member'
+        }
+    }).
+    state('knowledge-base.statistics', {
+        url: '/statistics',
+        templateUrl: 'app/partials/knowledge-base/statistics.html',
+        parent: 'knowledge-base',
+        controller: function($scope) {
+            $scope.totalMembers();
+        }
+    }).
+    state('members', {
+        url: '/members',
+        controller: 'memberCtrl',
+        templateUrl: 'app/partials/members/index.html'
+    }).
+    state('members.list', {
+        url: '/list',
+        templateUrl: 'app/partials/members/list.html',
+        parent: 'members',
+        controller: function($rootScope,$scope) {
+            $rootScope.title = 'Members List';
+            $scope.getMembers();
+        }
+    }).
+    state('members.add', {
+        url: '/add',
+        templateUrl: 'app/partials/members/form.html',
+        parent: 'members',
+        controller: function($rootScope) {
+            $rootScope.title = 'Register Member'
+        }
+    }).
+    state('members.view', {
+        url: '/view',
+        templateUrl: 'app/partials/members/form.html',
+        parent: 'members',
+        controller: function($rootScope) {
+            $rootScope.title = 'View Member'
+        }
+    }).
+    state('members.statistics', {
+        url: '/statistics',
+        templateUrl: 'app/partials/members/statistics.html',
+        parent: 'members',
+        controller: function($scope) {
+            $scope.totalMembers();
+        }
+    })
+
+});
+
+
+
+//app.config(function ($routeProvider) {
+//    $routeProvider.
+//    when("/members", {
+//        templateUrl: "app/crud_partials/members.html",
+//        controller: "memberCtrl"
+//    }).
+//    when("/benefits", {
+//        templateUrl: "app/crud_partials/benefits.html",
+//        controller: "benefitsCtrl"
+//    }).
+//    when("/claims", {
+//        templateUrl: "app/crud_partials/claims.html",
+//        controller: "claimsCtrl"
+//    }).
+//    when("/provider", {
+//        templateUrl: "app/crud_partials/provider.html",
+//        controller: "providerCtrl"
+//    }).
+//    when("/services", {
+//        templateUrl: "app/crud_partials/services.html",
+//        controller: "servicesCtrl"
+//    }).
+//    otherwise({
+//        redirectTo: '/'
+//    }); });
